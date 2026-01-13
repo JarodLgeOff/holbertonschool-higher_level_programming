be94b0778424a06e08891daa42ab1d244d88aeb9
JarodLge <contact.jarodlge@gmail.com>
2026-01-13 11:21:34 +0100

    fix: Correct last digit calculation for negative numbers in 1-last_digit.py

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -2,7 +2,11 @@
 import random
 number = random.randint(-10000, 10000)
 
-last_d = number % 10
+if number > 0:
+    last_d = number % 10
+else:
+    last_d = number % -10
+
 
 if last_d > 5:
     print(f"Last digit of {number} is {last_d} and is greater than 5")
