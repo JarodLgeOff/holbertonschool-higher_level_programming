74bbb9474aa434819a20b4cee5462355599e11f4
JarodLge <contact.jarodlge@gmail.com>
2026-01-23 17:20:52 +0100

    doc: Add docstring to clarify the purpose of the matrix_divided function

--- python-test_driven_development/2-matrix_divided.py
+++ python-test_driven_development/2-matrix_divided.py
@@ -1,4 +1,9 @@
 #!/usr/bin/python3
+"""
+Divides all elements of a matrix by a given divisor.
+"""
+
+
 def matrix_divided(matrix, div):
     """Divides all elements of a matrix by a given divisor.
 
