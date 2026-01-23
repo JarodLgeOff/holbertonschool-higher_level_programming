1e49c3d1a9a0b8c62c09803fd0691629788f8917
JarodLge <contact.jarodlge@gmail.com>
2026-01-23 16:10:45 +0100

    feat: Update docstring for add_integer function to clarify arguments and return value

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -1,6 +1,20 @@
 #!/usr/bin/python3
+"""
+Adds two integers or floats and returns an integer.
+"""
+
 
 def add_integer(a, b=98):
+    """
+    Adds two integers or floats.
+    Args:
+        a (int or float): first number.
+        b (int or float): second number.
+    Returns:
+        int: the addition of a and b.
+    Raises:
+        TypeError: If a or b is not an integer or float.
+    """
     if not isinstance(a, (int, float)):
         raise TypeError("a must be an integer")
 
