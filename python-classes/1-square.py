3f6e0acb2db727d9b6f83c0e3149ef17b0f8b432
JarodLge <contact.jarodlge@gmail.com>
2026-01-27 13:55:31 +0100

    feat: Implement Square class with size initialization

---
+++ python-classes/1-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Define a class Square."""
+
+
+class Square:
+
+    """Initialize a Square instance with a given size."""
+    def __init__(self, size):
+        self.__size = size
