529ac58d2219a630aec329b30f4160445cd401f8
JarodLge <contact.jarodlge@gmail.com>
2026-01-29 21:26:57 +0100

    feat: Refactor Rectangle class constructor to use property setters for width and height

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -6,8 +6,8 @@ class Rectangle:
     """Represent a rectangle"""
     def __init__(self, width=0, height=0):
         """Initialize a Rectangle instance with width and height"""
-        self.__width = width
-        self.__height = height
+        self.width = width
+        self.height = height
 
     @property
     def width(self):
@@ -33,4 +33,6 @@ class Rectangle:
         """Set the height of the rectangle"""
         if not isinstance(value, int):
             raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
         self.__height = value
