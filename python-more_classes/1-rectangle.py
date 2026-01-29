827ffccf4416791a156b2d72a758c125ea9f99fa
JarodLge <contact.jarodlge@gmail.com>
2026-01-29 21:22:41 +0100

    feat: Add type validation for width and height setters in Rectangle class

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -17,6 +17,10 @@ class Rectangle:
     @width.setter
     def width(self, value):
         """Set the width of the rectangle"""
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
         self.__width = value
 
     @property
@@ -27,4 +31,6 @@ class Rectangle:
     @height.setter
     def height(self, value):
         """Set the height of the rectangle"""
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
         self.__height = value
