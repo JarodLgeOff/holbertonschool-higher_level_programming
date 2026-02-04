55aa9fc723c64c96b463c3ce18f5bb1b359a6f61
JarodLge <contact.jarodlge@gmail.com>
2026-02-04 17:05:17 +0100

    feat: Add Square class inheriting from Rectangle with area calculation

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -2,20 +2,7 @@
 """Module defining BaseGeometry class"""
 
 
-class BaseGeometry:
-    """BaseGeometry class with area method"""
-    def area(self):
-        """Raises an exception for unimplemented area method"""
-        raise Exception("area() is not implemented")
-    pass
-
-    """Validates that value is a positive integer"""
-    def integer_validator(self, name, value):
-        """Validates that value is a positive integer"""
-        if not isinstance(value, int):
-            raise TypeError(f"{name} must be an integer")
-        if value <= 0:
-            raise ValueError(f"{name} must be greater than 0")
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
 class Rectangle(BaseGeometry):
