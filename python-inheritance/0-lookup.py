318cc7675f08317ae1136051512523b587cea33b
JarodLge <contact.jarodlge@gmail.com>
2026-02-04 09:51:58 +0100

    fix: Correct docstring formatting in lookup function for parameter description

--- python-inheritance/0-lookup.py
+++ python-inheritance/0-lookup.py
@@ -8,6 +8,6 @@ def lookup(obj):
     """
     Returns the list of available attributes and methods of an object:
 
-    param obj: The object to lookup
+    :param obj: The object to lookup
     """
     return dir(obj)
