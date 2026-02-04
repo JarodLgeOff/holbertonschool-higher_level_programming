e6952bbbd6559411bdf6c30cd9e64305674d6151
JarodLge <contact.jarodlge@gmail.com>
2026-02-04 09:49:31 +0100

    fix: Update docstring in lookup function for clarity and parameter description

--- python-inheritance/0-lookup.py
+++ python-inheritance/0-lookup.py
@@ -3,5 +3,8 @@
 
 
 def lookup(obj):
-    """Returns the list of available attributes and methods of an object"""
+    """
+    Returns the list of available attributes and methods of an object
+    param obj: The object to lookup
+    """
     return dir(obj)
