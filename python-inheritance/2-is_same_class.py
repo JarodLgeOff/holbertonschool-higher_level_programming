ba7e61b6b0341349e73fe445950df428bd276844
JarodLge <contact.jarodlge@gmail.com>
2026-02-04 10:37:16 +0100

    fix: Update docstring in is_same_class function for clarity

--- python-inheritance/2-is_same_class.py
+++ python-inheritance/2-is_same_class.py
@@ -3,4 +3,9 @@
 
 
 def is_same_class(obj, a_class):
+    """Return True if the object is exactly an instance of the specified class
+    Args:
+        obj: The object to check
+        a_class: The class to match the type of obj to
+    """
     return type(obj) is a_class
