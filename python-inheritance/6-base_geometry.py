c23ea514020f9c7561185415dcf6d6bd79907317
JarodLge <contact.jarodlge@gmail.com>
2026-02-04 11:14:14 +0100

    feat: Add BaseGeometry class and main script for testing area method

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+class BaseGeometry:
+    """BaseGeometry class with area method"""
+    def area(self):
+        """Raises an exception for unimplemented area method"""
+        raise Exception("area() is not implemented")
+    pass
