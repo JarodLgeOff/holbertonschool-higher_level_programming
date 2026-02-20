19cc368593c029792b3930ca3a6635a1d5a94cfe
JarodLge <contact.jarodlge@gmail.com>
2026-02-20 10:51:41 +0100

    fix: Correct capitalization in 404 error message

--- restful-api/task_03_http_server.py
+++ restful-api/task_03_http_server.py
@@ -44,7 +44,7 @@ class SimpleHTTPRequestHandler(http.server.BaseHTTPRequestHandler):
             self.send_response(404)
             self.send_header('Content-type', 'text/plain')
             self.end_headers()
-            self.wfile.write(b"Endpoint Not Found")
+            self.wfile.write(b"Endpoint not found")
 
 
 with socketserver.TCPServer(("", port), SimpleHTTPRequestHandler) as httpd:
