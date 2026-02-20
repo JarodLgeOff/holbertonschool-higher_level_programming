d8829a30cedcaf906ac17e5aa0b73ff8cfd36319
JarodLge <contact.jarodlge@gmail.com>
2026-02-20 10:23:34 +0100

    feat: Refactor fetch_and_save_posts to save structured posts to CSV

--- restful-api/task_02_requests.py
+++ restful-api/task_02_requests.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python3
 """Task 2: Fetch and Print Posts from an API"""
-
 import requests
+import csv
 
 
 def fetch_and_print_posts():
-    """Fetches posts from the API and prints their titles."""
+    """Fetches posts from the API and prints their titles"""
     url = 'https://jsonplaceholder.typicode.com/posts'
     response = requests.get(url)
     if response.status_code == 200:
@@ -16,13 +16,24 @@ def fetch_and_print_posts():
 
 
 def fetch_and_save_posts():
-    """Fetches posts from the API and saves them to a file."""
+    """Fetches posts from the API and saves them to a file"""
     url = 'https://jsonplaceholder.typicode.com/posts'
     response = requests.get(url)
     if response.status_code == 200:
         posts = response.json()
+
+        structured_posts = [
+            {
+                'id': post['id'],
+                'title': post['title'],
+                'body': post['body']
+                } for post in posts
+        ]
         with open('posts.csv', 'w', newline='', encoding='utf-8') as f:
-            for post in posts:
-                f.write(f"{post['title']}\n")
+            fieldnames = ['id', 'title', 'body']
+            writer = csv.DictWriter(f, fieldnames=fieldnames)
+
+            writer.writeheader()
+            writer.writerows(structured_posts)
     else:
         print(f"Failed to fetch posts. Status code: {response.status_code}")
