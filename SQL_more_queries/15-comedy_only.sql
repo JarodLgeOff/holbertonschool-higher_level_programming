52ce20c39722102c83f4b09c65e2a5af4020951d
JarodLge <contact.jarodlge@gmail.com>
2026-03-04 23:55:22 +0100

    fix: Correct table reference for tv_shows in SELECT and ORDER BY clauses of comedy SQL script

--- SQL_more_queries/15-comedy_only.sql
+++ SQL_more_queries/15-comedy_only.sql
@@ -1,9 +1,9 @@
 -- Write a SQL script that lists all the TV shows in the database that have the genre “Comedy”
-SELECT tv_show.title
+SELECT tv_shows.title
 FROM tv_shows
 JOIN tv_show_genres 
 ON tv_shows.id = tv_show_genres.tv_show_id
 JOIN tv_genres
 ON tv_genres.id = tv_show_genres.genre_id
 WHERE tv_genres.name = 'Comedy'
-ORDER BY tv_show.title ASC;
+ORDER BY tv_shows.title ASC;
