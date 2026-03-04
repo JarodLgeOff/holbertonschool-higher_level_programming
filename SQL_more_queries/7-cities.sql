2ab6f77f1179a6df2965a5f4b22646ed28389ea0
JarodLge <contact.jarodlge@gmail.com>
2026-03-04 22:18:16 +0100

    feat: Create SQL script to define cities table in hbtn_0d_usa database

---
+++ SQL_more_queries/7-cities.sql
@@ -0,0 +1,10 @@
+-- Create a database named hbtn_0d_usa and a table named cities with the following columns
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+USE hbtn_0d_usa;
+
+CREATE TABLE IF NOT EXISTS cities (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    state_id INT,
+    FOREIGN KEY (state_id) REFERENCES states(id)
+);
