9722b8e53c69e3611d13e869f2a4cd2f3b741048
JarodLge <contact.jarodlge@gmail.com>
2026-03-05 00:09:16 +0100

    fix: Correct table reference for tv_shows in ORDER BY clause of shows_by_genre SQL script

--- SQL_more_queries/16-shows_by_genre.sql
+++ SQL_more_queries/16-shows_by_genre.sql
@@ -5,4 +5,4 @@ LEFT JOIN tv_show_genres
 ON tv_shows.id = tv_show_genres.tv_show_id
 LEFT JOIN tv_genres
 ON tv_show_genres.genre_id = tv_genres.id
-ORDER BY tv_show.title ASC, tv_genres.name ASC;
+ORDER BY tv_shows.title ASC, tv_genres.name ASC;
