2c197650c9ba43d2816d8a4de6df58ef90670b62
JarodLge <contact.jarodlge@gmail.com>
2026-03-04 22:05:47 +0100

    feat: Create SQL script to define states table in hbtn_0d_usa database

---
+++ SQL_more_queries/6-states.sql
@@ -0,0 +1,7 @@
+-- Create a database named hbtn_0d_usa and a table named states with the following columns
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+USE hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS states (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(255) NOT NULL
+);
