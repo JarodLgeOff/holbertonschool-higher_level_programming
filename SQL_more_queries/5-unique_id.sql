11ca8e27935bcc4ed7c4c229da0195275503cccc
JarodLge <contact.jarodlge@gmail.com>
2026-03-04 22:07:08 +0100

    feat: Update SQL script to create unique_id table with id and name columns

--- SQL_more_queries/5-unique_id.sql
+++ SQL_more_queries/5-unique_id.sql
@@ -1,5 +1,5 @@
--- Create a table named force_name with the following columns
-CREATE TABLE IF NOT EXISTS force_name (
+-- Create a table named unique_id with the following columns
+CREATE TABLE IF NOT EXISTS unique_id (
     id INT DEFAULT 1 UNIQUE,
     name VARCHAR(255) NOT NULL
 );
