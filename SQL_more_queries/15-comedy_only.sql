94f9a02cadf9fcdf620d3425b1a1df6f10fffa8c
JarodLge <contact.jarodlge@gmail.com>
2026-03-04 23:58:46 +0100

    fix: Correct table reference for tv_show_genres in JOIN clause of comedy SQL script

--- SQL_more_queries/15-comedy_only.sql
+++ SQL_more_queries/15-comedy_only.sql
@@ -2,8 +2,8 @@
 SELECT tv_shows.title
 FROM tv_shows
 INNER JOIN tv_show_genres 
-ON tv_shows.id = tv_show_genres.tv_show_id
+ON tv_shows.id = tv_show_genres.show_id
 INNER JOIN tv_genres
 ON tv_genres.id = tv_show_genres.genre_id
-WHERE tv_genres.name = "Comedy" and tv_show_genres.genre_id IS NOT NULL
+WHERE tv_genres.name = "Comedy" AND tv_show_genres.genre_id IS NOT NULL
 ORDER BY tv_shows.title ASC;
