1272537b9a1d412b2307f0ba95c84d54739b26cb
JarodLge <contact.jarodlge@gmail.com>
2026-03-04 22:52:23 +0100

    fix: Correct SQL script to use the proper column names for genre_id and show_id

--- SQL_more_queries/10-genre_id_by_show.sql
+++ SQL_more_queries/10-genre_id_by_show.sql
@@ -1,5 +1,6 @@
 -- Write a SQL script that lists all the shows and their genres
-SELECT tv_show.title, tv_show.genres.genre_id
+SELECT tv_show.title, tv_show_genres.genre_id
 FROM tv_show
-JOIN tv_show_genres ON tv_show.id = tv_show_genres.tv_show_id
+JOIN tv_show_genres 
+ON tv_show.id = tv_show_genres.show_id
 ORDER BY tv_show.title ASC, tv_show_genres.genre_id ASC;
