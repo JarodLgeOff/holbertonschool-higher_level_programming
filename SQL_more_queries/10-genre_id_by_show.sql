89759ba456ea66bb0af53ec2467af04b32859f71
JarodLge <contact.jarodlge@gmail.com>
2026-03-04 22:53:23 +0100

    fix: Correct SQL script to use consistent table names for shows

--- SQL_more_queries/10-genre_id_by_show.sql
+++ SQL_more_queries/10-genre_id_by_show.sql
@@ -1,6 +1,6 @@
 -- Write a SQL script that lists all the shows and their genres
-SELECT tv_show.title, tv_show_genres.genre_id
-FROM tv_show
-JOIN tv_show_genres 
-ON tv_show.id = tv_show_genres.show_id
-ORDER BY tv_show.title ASC, tv_show_genres.genre_id ASC;
+SELECT tv_shows.title, tv_show_genres.genre_id
+FROM tv_shows
+JOIN tv_show_genres
+ON tv_shows.id = tv_show_genres.show_id
+ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
