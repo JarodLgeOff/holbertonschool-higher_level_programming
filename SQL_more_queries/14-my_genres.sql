fa75cb84492dadb018aab7e9886daa8447839733
JarodLge <contact.jarodlge@gmail.com>
2026-03-04 23:44:05 +0100

    fix: Update JOIN syntax for clarity and consistency in SQL query for "Dexter" genres

--- SQL_more_queries/14-my_genres.sql
+++ SQL_more_queries/14-my_genres.sql
@@ -1,9 +1,9 @@
 -- Write a SQL query that lists all genres of the TV show “Dexter” in alphabetical order
 SELECT tv_genres.name
 FROM tv_genres
-JOIN tv_show_genres 
+INNER JOIN tv_show_genres 
 ON tv_genres.id = tv_show_genres.genre_id
-JOIN tv_shows
-ON tv_shows.id = tv_show_genres.tv_show_id
-WHERE tv_shows.title = 'Dexter' and tv_show_genres.genre_id IS NOT NULL
+INNER JOIN tv_shows 
+ON tv_show_genres.show_id = tv_shows.id
+WHERE tv_shows.title = "Dexter" AND tv_show_genres.genre_id IS NOT NULL
 ORDER BY tv_genres.name ASC;
