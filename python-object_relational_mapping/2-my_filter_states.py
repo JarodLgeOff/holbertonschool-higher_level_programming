bf2f2155cd87c36d0444ab8a81e695ecd07b09a3
JarodLge <contact.jarodlge@gmail.com>
2026-03-06 14:14:01 +0100

    fix: Correct SQL query ordering in 2-my_filter_states.py

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -20,7 +20,7 @@ if __name__ == "__main__":
     cur = db.cursor()
     query = (
         "SELECT * FROM states WHERE name = '{}' "
-        "ORDER BY id ASC".format(state_name)
+        "ORDER BY states.id ASC".format(state_name)
     )
 
     cur.execute(query)
