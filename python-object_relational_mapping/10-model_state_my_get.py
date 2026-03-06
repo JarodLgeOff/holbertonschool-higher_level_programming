08d9ab8554ffa98a03e00b77f0b651ad1c8353c2
JarodLge <contact.jarodlge@gmail.com>
2026-03-06 18:56:05 +0100

    fix: Correct syntax error in query for state retrieval in 10-model_state_my_get.py

--- python-object_relational_mapping/10-model_state_my_get.py
+++ python-object_relational_mapping/10-model_state_my_get.py
@@ -19,7 +19,7 @@ if __name__ == "__main__":
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    states = session.query(State)filter(State.name == state_name).first()
+    states = session.query(State).filter(State.name == state_name).first()
 
     if states:
         print(states.id)
