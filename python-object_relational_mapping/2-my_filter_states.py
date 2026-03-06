1d6673170483c66cbc20179cfd56c19069775f5a
JarodLge <contact.jarodlge@gmail.com>
2026-03-06 14:17:29 +0100

    fix: Improve SQL query formatting for better readability in 2-my_filter_states.py

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -19,7 +19,8 @@ if __name__ == "__main__":
 
     cur = db.cursor()
     query = (
-        "SELECT * FROM states WHERE name = BINARY '{}' ORDER BY states.id ASC".format(state_name)
+        "SELECT * FROM states WHERE name = BINARY '{}' "
+        "ORDER BY states.id ASC".format(state_name)
     )
 
     cur.execute(query)
