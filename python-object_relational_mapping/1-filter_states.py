74d519901f9eb5aceb1cab1d4ceea018576cdbc5
JarodLge <contact.jarodlge@gmail.com>
2026-03-06 13:39:25 +0100

    fix: Correct SQL query ordering in 1-filter_states.py

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -18,12 +18,14 @@ if __name__ == "__main__":
     )
 
     cur = db.cursor()
+
     cur.execute(
         "SELECT * FROM states "
-        "WHERE name LIKE 'N%' ORDER BY id ASC"
+        "WHERE name LIKE 'N%' ORDER BY states.id ASC"
         )
 
     rows = cur.fetchall()
+
     for row in rows:
         print(row)
 
