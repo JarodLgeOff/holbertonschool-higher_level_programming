43325a0eb5cfad3981daf8b78a03172a962ef19c
JarodLge <contact.jarodlge@gmail.com>
2026-03-06 19:23:51 +0100

    fix: Update file permissions and improve docstring formatting in model_city.py

--- python-object_relational_mapping/model_city.py
+++ python-object_relational_mapping/model_city.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
 """This module defines the City class that maps to the cities database."""
-from model_state import Base, State
 from sqlalchemy import Column, Integer, String, ForeignKey
+from model_state import Base
 
 
 class City(Base):
-    """City class that inherits from Base and links to the table cities"""
+    """Represents a city."""
+    __tablename__ = "cities"
 
-    __tablename__ = 'cities'
-    id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
+    id = Column(Integer, primary_key=True, nullable=False)
     name = Column(String(128), nullable=False)
-    state_id = Column(Integer, ForeignKey('states.id'), nullable=False)
+    state_id = Column(Integer, ForeignKey("states.id"), nullable=False)
