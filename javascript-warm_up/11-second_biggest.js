ffc1e9f63f0be551c12119d070acedde5cfaf6a7
JarodLge <contact.jarodlge@gmail.com>
2026-03-20 19:27:21 +0100

    style: Fix indentation and add newline at end of file in 11-second_biggest.js

--- javascript-warm_up/11-second_biggest.js
+++ javascript-warm_up/11-second_biggest.js
@@ -3,7 +3,7 @@
 const args = process.argv.slice(2).map(Number).sort((a, b) => b - a);
 
 if (args.length < 2) {
-    console.log(0);
+  console.log(0);
 } else {
-    console.log(args[1]);
+  console.log(args[1]);
 }
