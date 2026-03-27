fc59f960a7ed739d3506da4299f5da76d3de7de4
JarodLge <contact.jarodlge@gmail.com>
2026-03-27 01:46:20 +0100

    feat: Add iitems.json file containing initial items list

--- python-server_side_rendering/task_01_jinja.py
+++ python-server_side_rendering/task_01_jinja.py
@@ -18,11 +18,5 @@ def contact():
     return render_template('contact.html')
 
 
-@app.route('/items')
-def items():
-    with open('items.json', 'r') as f:
-        data = json.load(f)
-    return render_template('items.html', items=data["items"])
-
 if __name__ == '__main__':
     app.run(debug=True, port=5000)
